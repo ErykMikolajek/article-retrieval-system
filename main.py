d6e3eddcb34a60bae1e9c223fc3fd5bf5978a503
Eryk Mikołajek <mikolajek.eryk@gmail.com>
2024-04-10 22:52:11 +0200

    added model structure

--- main.py
+++ main.py
@@ -1,57 +1,108 @@
 from vector_database import generate_database
 
-from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig, pipeline
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 from langchain_community.llms import HuggingFacePipeline
 from langchain.prompts import PromptTemplate
-# from langchain_community.vectorstores import FAISS
+from langchain.schema.runnable import RunnablePassthrough
+from langchain.chains import LLMChain
+
+from typing import Any
+from uuid import UUID
+from tqdm.auto import tqdm
+from langchain_core.callbacks import BaseCallbackHandler
+from langchain_core.outputs import LLMResult
+
+import warnings
+warnings.simplefilter('ignore')
+
+
+class BatchCallback(BaseCallbackHandler):
+    def __init__(self, total: int):
+        super().__init__()
+        self.count = 0
+        self.progress_bar = tqdm(total=total, desc="Generating tokens")
+
+    def on_llm_end(self, response: LLMResult, *, run_id: UUID, parent_run_id: UUID | None = None, **kwargs: Any):
+        self.count += 1
+        self.progress_bar.update(1)
 
 
 class RetrievalModel:
-    def __init__(self, model_name: str):
-        # self.vector_database = generate_database(database_path)
-        # self.bnb_config = BitsAndBytesConfig(
-        #     load_in_4bit=True,
-        #     bnb_4bit_quant_type="nf4",
-        #     bnb_4bit_compute_dtype="float16",
-        #     bnb_4bit_use_double_quant=False,
-        # )
-        self.model = AutoModelForCausalLM.from_pretrained(
-            model_name,
-            quantization_config=self.bnb_config,
-            device_map="auto")
+    def __init__(self, model_name, database_path='articles_indexed', max_tokens=100):
+        print("Loading model:")
+        self.model = AutoModelForCausalLM.from_pretrained(model_name)
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        self.database_path = database_path
+        self.vector_db = self.load_vector_db()
+        self.max_tokens = max_tokens
 
-        self.text_generation_pipeline = pipeline(
+        self.text_generation_pipeline = self.setup_text_generation_pipeline()
+        self.mistral_llm = self.setup_mistral_llm()
+        self.prompt = self.setup_prompt_template()
+        self.llm_chain = self.setup_llm_chain()
+
+    def setup_text_generation_pipeline(self):
+        return pipeline(
             model=self.model,
             tokenizer=self.tokenizer,
             task="text-generation",
             temperature=0.2,
             repetition_penalty=1.1,
             return_full_text=True,
-            max_new_tokens=300
+            max_new_tokens=self.max_tokens
         )
 
-        mistral_llm = HuggingFacePipeline(pipeline=self.text_generation_pipeline)
-        self.prompt_template = """
-        [INST] 
+    def setup_mistral_llm(self):
+        return HuggingFacePipeline(pipeline=self.text_generation_pipeline)
+
+    def setup_prompt_template(self):
+        prompt_template = """
+        [INST]
         Answer the question based on the following context:
         {context}
         [/INST]
 
         Question:
         {question} 
-         """
-
-        # Create prompt from prompt template
-        self.prompt = PromptTemplate(
+        """
+        return PromptTemplate(
             input_variables=["context", "question"],
-            template=self.prompt_template,
+            template=prompt_template,
         )
-    def invoke(self, query: str):
-        pass
+
+    def setup_llm_chain(self):
+        return LLMChain(llm=self.mistral_llm, prompt=self.prompt)
+
+    def load_vector_db(self):
+        return generate_database(self.database_path)
+
+    def get_answer(self, local_query):
+        # cb = BatchCallback(len(local_query))
+        print("Generating response:")
+        if self.vector_db:
+            retriever = self.vector_db.as_retriever(
+                search_type="similarity",
+                search_kwargs={'k': 1}
+            )
+            rag_chain = (
+                    {"context": retriever, "question": RunnablePassthrough()}
+                    | self.llm_chain
+            )
+            response = rag_chain.invoke(local_query)
+        else:
+            response = self.llm_chain.run(local_query)
+
+        # cb.progress_bar.close()
+        return response
+
 
 if __name__ == '__main__':
-    vector_database = generate_database('medium.csv')
+    # name = 'TinyLlama/TinyLlama-1.1B-Chat-v1.0'
+    name = '../tiny_llama'
+    # name = '../mistral_model'
+    model = RetrievalModel(name, max_tokens=20)
+    query = "Tell me about Transformers."
+    answer = model.get_answer(query)
+    print(answer)
 
-    model = RetrievalModel('../mistral_model')
-    # print(vector_database)
+
